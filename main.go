b17f225ff8a3cd130b0dbddae33edb554552e0e2
Vitor <pro.vitoralmeida@gmail.com>
2022-12-14 19:07:42 -0300

    Add home page via template

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html charset=UTF-8")
-	fmt.Fprint(w, "<h1>Welcome to my fucking awesome site!</h1>")
+	tplPath := filepath.Join("templates", "home.tmpl")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
