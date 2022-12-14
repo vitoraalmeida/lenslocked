6d00142829c05cdc36357e1d75fc5206a5edae56
Vitor <pro.vitoralmeida@gmail.com>
2022-12-14 20:55:15 -0300

    Add 'must' function to reduce error handling

--- main.go
+++ main.go
@@ -13,22 +13,13 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.tmpl"))
-	if err != nil {
-		panic(err)
-	}
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.tmpl")))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.tmpl"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.tmpl")))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.tmpl"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.tmpl")))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -11,16 +11,6 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
-	return Template{
-		htmlTpl: tpl,
-	}, nil
-}
-
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html charset=UTF-8")
 	err := t.htmlTpl.Execute(w, data)
@@ -29,4 +19,25 @@ func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
+}
+
+// útil quando retornamos um tipo e um erro e só retornamos o tipo se não houver erro
+// sendo que faz sentido no contexto usar panic, pois a aplicação não pode executar
+// se não acontecer. Indicativo de uso é na função main, onde erros no meio dela
+// devem fazer o programa parar
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{
+		htmlTpl: tpl,
+	}, nil
 }
