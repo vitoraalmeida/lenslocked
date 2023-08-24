c7439e6c4950cb94804431bd2198c5ad3e0d1bf5
Vitor Almeida <pro.vitoralmeida@gmail.com>
2023-08-24 19:13:43 -0300

    Adiciona serviço de envio de emails

---
+++ models/email.go
@@ -0,0 +1,82 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/go-mail/mail"
+)
+
+const (
+	// DefaultSender is the default email address to send emails from.
+	DefaultSender = "support@lenslocked.com"
+)
+
+type SMTPConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
+func NewEmailService(config SMTPConfig) *EmailService {
+	es := EmailService{
+		dialer: mail.NewDialer(config.Host, config.Port, config.Username, config.Password),
+	}
+	return &es
+}
+
+type EmailService struct {
+	// DefaultSender is used as the default sender when one isn't provided for an
+	// email. This is also used in functions where the email is a predetermined,
+	// like the forgotten password email.
+	DefaultSender string
+
+	// unexported fields
+	dialer *mail.Dialer
+}
+
+type Email struct {
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
+}
+
+func (es *EmailService) Send(email Email) error {
+	msg := mail.NewMessage()
+	msg.SetHeader("To", email.To)
+	es.setFrom(msg, email)
+	msg.SetHeader("Subject", email.Subject)
+	switch {
+	case email.Plaintext != "" && email.HTML != "":
+		msg.SetBody("text/plain", email.Plaintext)
+		msg.AddAlternative("text/html", email.HTML)
+	case email.Plaintext != "":
+		msg.SetBody("text/plain", email.Plaintext)
+	case email.HTML != "":
+		msg.SetBody("text/html", email.HTML)
+	}
+	err := es.dialer.DialAndSend(msg)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
+
+// Used to set the sender of the message. The priority is:
+//   - email.From
+//   - EmailService.DefaultSender
+//   - DefaultSender (package const)
+func (es *EmailService) setFrom(msg *mail.Message, email Email) {
+	var from string
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+	msg.SetHeader("From", from)
+}
