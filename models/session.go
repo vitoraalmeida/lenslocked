eb39567dddaeabfa53fea4337e9716807ed9025d
Vitor Almeida <pro.vitoralmeida@gmail.com>
2023-08-24 13:34:11 -0300

    Usa query inner join para recuperar dados do usuário com base no token de sessão em uma query só

--- models/session.go
+++ models/session.go
@@ -69,22 +69,20 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 
 func (ss *SessionService) User(token string) (*User, error) {
 	tokenHash := ss.hash(token)
-	row := ss.DB.QueryRow(
-		`SELECT user_id FROM sessions WHERE token_hash=$1;`,
-		tokenHash)
-	var userID int
-	err := row.Scan(&userID)
+	row := ss.DB.QueryRow(`
+	SELECT
+		users.id,
+		users.email,
+		users.password_hash
+	FROM
+		sessions
+		JOIN users ON users.id = sessions.user_id
+	WHERE
+		sessions.token_hash = $1;`, tokenHash)
+	var user User
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
 	if err != nil {
 		return nil, fmt.Errorf("user: %w", err)
-	}
-
-	var user User
-
-	row = ss.DB.QueryRow(
-		`SELECT email FROM users WHERE id=$1`, userID)
-	err = row.Scan(&user.Email)
-	if err != nil {
-		return nil, fmt.Errorf("authenticate: %w", err)
 	}
 
 	return &user, nil
--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -8,3 +8,42 @@ INSERT INTO
     sessions (user_id, token_hash)
 VALUES
     ($ 1, $ 2) RETURNING id;
+
+-- estabelecer relacionamento com outra tabela
+-- faz com que ids que colocarmos nesse tabela de fato precisam existir na tabela de usuário e se deletarmos um usuário, não podemos ter uma sessão para aquele user id pois uma sessão que usa aquele user_id impede que o usuário seja deletado, então para deletar o usuário precisamos deletar antes a sessão
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE REFERENCES users (id),
+    token_hash TEXT UNIQUE NOT NULL
+);
+
+-- ou 
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE,
+    token_hash TEXT UNIQUE NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users (id)
+);
+
+-- para adicionar uma relação a uma tabela já existente
+ALTER TABLE
+    sessions
+ADD
+    CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id);
+
+-- Para deletar sessões quando o usuário relacionado for deletado
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
+    token_hash TEXT UNIQUE NOT NULL
+);
+
+SELECT
+    users.id,
+    users.email,
+    users.password_hash
+FROM
+    sessions
+    JOIN users ON users.id = sessions.user_id
+WHERE
+    sessions.token_hash = $ 1
